825761d154f45fedd3c63382f8f3cf38024ea69a
larihthebest <larissasprenger2006@gmail.com>
2024-05-02 22:01:27 -0300

    Tela inicial

--- app/src/main/java/com/example/projeto_appi/MainActivity.kt
+++ app/src/main/java/com/example/projeto_appi/MainActivity.kt
@@ -3,10 +3,19 @@ package com.example.projeto_appi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.projeto_appi.screens.InfiniteScreen
 import com.example.projeto_appi.ui.theme.Projeto_AppITheme
 
@@ -23,4 +32,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
