d1d96327de9af9cd37d392abc68298fe90757ff6
larihthebest <larissasprenger2006@gmail.com>
2024-05-02 21:47:44 -0300

    Tela inicial

--- app/src/main/java/com/example/projeto_appi/MainActivity.kt
+++ app/src/main/java/com/example/projeto_appi/MainActivity.kt
@@ -1,33 +1,14 @@
 package com.example.projeto_appi
 
-import android.content.IntentSender.OnFinished
 import android.os.Bundle
-import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.projeto_appi.screens.InfiniteScreen
 import com.example.projeto_appi.ui.theme.Projeto_AppITheme
-import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
